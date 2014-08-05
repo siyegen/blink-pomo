b4cb851f9d4948744c59831ba8629f925281074c
David Tomberlin <siyegen@gmail.com>
2014-08-04 23:23:41 -0700

    server up index.html, but that is it

--- src/blink-pomo/main.go
+++ src/blink-pomo/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("blink-pomo: Pretty lights and productivity")
+
+	http.Handle("/", http.FileServer(http.Dir("assets")))
+
+	http.ListenAndServe(":9913", nil)
 }
